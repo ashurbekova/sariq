f02d20381690159c6a6be343200ef4f0e23096d7
ashurbekova <84651485+ashurbekova@users.noreply.github.com>
2021-05-23 10:14:41 +0600

    Add files via upload

---
+++ prog2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 12:29:25 2021
+
+@author: User
+"""
+
+print('"Nexia" , "Tico" ,  \'Damas\'  ko\'rganlar qilar havas')
+
+
+      
+      
+      
+      
+
+      
+      
+      
+      
+       
+      
+       
+      
+      
+      
+      
